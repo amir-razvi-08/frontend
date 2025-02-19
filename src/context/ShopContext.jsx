26e79da73874dd1b8928746b811b3ee336be9304
Amir Razvi <amirrazvi8@gmail.com>
2025-02-19 21:06:18 +0530

    update server URL to production endpoint

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -19,7 +19,7 @@ const ShopContextProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
     const [amount, setAmount] = useState(0);
     const [orderItems, setOrderItems] = useState([]);
-    const serverURL = "http://localhost:4000/api/v1";
+    const serverURL = "https://luxora-fashion.vercel.app/api/v1";
 
     const token = localStorage.getItem("authToken");
 
